147dc83b8f33bac8d2cae3c91c4ef741084e1c56
Koutarou Chikuba <miz404@gmail.com>
2011-11-16 17:04:00 +0900

    add file

---
+++ public/game.coffee
@@ -0,0 +1,354 @@
+Array::remove = (obj)-> @splice(@indexOf(obj),1)
+Array::size = ()-> @length
+Array::first = ()-> @[0]
+Array::last = ()-> @[@length-1]
+Array::each = Array::forEach
+{cos,sin,sqrt,abs} = Math
+
+Color =
+  Rンed : "rgb(255,0,0)"
+  Blue : "rgb(0,0,255)"
+  Green : "rgb(0,255,0)"
+  White : "rgb(255,255,255)"
+  Black : "rgb(0,0,0)"
+  i : (r,g,b)->
+    "rgb(#{r},#{g},#{b})"
+
+#===== CanvasRenderingContext2D =====
+Canvas = CanvasRenderingContext2D
+Canvas::init = (color=Color.i(255,255,255),alpha=1)->
+  @beginPath()
+  @strokeStyle = color
+  @fillStyle = color
+  @globalAlpha = alpha
+
+Canvas::initText = (size=10,font='Arial')->
+  @font = "#{size}pt #{font}"
+
+Canvas::drawLine = (x,y,dx,dy)->
+  @moveTo x,y
+  @lineTo x+dx,y+dy
+  @stroke()
+Canvas::drawPath = (fill,path)->
+  [sx,sy] = path.shift()
+  @moveTo sx,sy
+  while path.size() > 0
+    [px,py] = path.shift()
+    @lineTo px,py
+  @lineTo sx,sy
+  if fill then @fill() else @stroke()
+
+Canvas::drawDiffPath = (fill,path)->
+  [sx,sy] = path.shift()
+  @moveTo sx,sy
+  [px,py] = [sx,sy]
+  while path.size() > 0
+    [dx,dy] = path.shift()
+    [px,py] = [px+dx,py+dy]
+    @lineTo px,py
+  @lineTo sx,sy
+  if fill then @fill() else @stroke()
+
+Canvas::drawLine = (x,y,dx,dy)->
+  @moveTo x,y
+  @lineTo x+dx,y+dy
+  @stroke()
+
+Canvas::drawDLine = (x1,y1,x2,y2)->
+  @moveTo x1,y1
+  @lineTo x2,y2
+  @stroke()
+
+Canvas::drawArc = (fill , x,y,size,from=0, to=Math.PI*2,reverse=false)->
+  @arc( x, y, size ,from ,to ,reverse)
+  if fill then @fill() else @stroke()
+#===== UtilClass =====
+Util = {}
+Util.prototype =
+  extend : (obj, mixin) ->
+    obj[name] = method for name, method of mixin
+    obj
+
+  include : (klass, mixin) ->
+    Util::extend klass.prototype, mixin
+
+  dup : (obj)->
+    f = ()->
+    f.prototype = obj
+    new f
+include = Util::include
+
+class Sprite
+  constructor: (@x=0,@y=0,@scale=10) ->
+  render: (g)->
+    g.beginPath()
+    g.arc(@x,@y, 15 ,0,Math.PI*2,true)
+    g.stroke()
+
+  get_distance: (target)->
+    xd = Math.pow (@x-target.x) ,2
+    yd = Math.pow (@y-target.y) ,2
+    return Math.sqrt xd+yd
+
+  get_relative:(cam)->
+    pos =
+      vx : 320 + @x - cam.x
+      vy : 240 + @y - cam.y
+
+  find_obj:(group_id,targets, range)->
+    targets.filter (t)=>
+      t.group is group_id and @get_distance(t) < range
+
+class ImageSprite
+  constructor:(src,@size)->
+    @img = new Image
+    @img.src = src
+  draw:(g)->
+    g.drawImage(@img, x,y)
+
+class CanvasSprite
+  constructor:(@size=32)->
+    buffer = document.createElement('canvas')
+    buffer.width = buffer.height = @size
+    @shape buffer.getContext('2d')
+    @img = new Image
+    @img.src = buffer.toDataURL()
+
+  p2ism : (x,y)->
+    [(x+y)/2
+     (x-y)/4
+    ]
+
+  draw:(g,x,y)->
+    dx = dy = ~~(@size/2)
+    g.drawImage(@img, x-dx,y-dy)
+
+class CharSprite extends CanvasSprite
+  shape: (g)->
+    cx = cy = 16 
+    g.init Color.i(64,255,64)
+    g.arc( cx , cy-7 ,3 ,0 , 2*Math.PI )
+    g.fill()
+
+    g.beginPath()
+    g.moveTo cx,cy
+    g.lineTo(xx,yy) for [xx,yy] in [
+      [cx-4  , cy-3 ]
+      [cx+4  , cy-3 ]
+    ]
+    g.lineTo cx,cy
+    g.fill()
+
+class PlayerSprite extends CanvasSprite
+  shape: (g)->
+    cx = cy = 16 
+    g.init Color.i(64,64,255)
+    g.arc( cx , cy-7 ,3 ,0 , 2*Math.PI )
+    g.fill()
+
+    g.beginPath()
+    g.moveTo cx,cy
+    g.lineTo(xx,yy) for [xx,yy] in [
+      [cx-4  , cy-3 ]
+      [cx+4  , cy-3 ]
+    ]
+    g.lineTo cx,cy
+    g.fill()
+
+class MonsterSprite extends CanvasSprite
+  shape: (g,color)->
+    cx = cy = 16 
+    g.init color or Color.i(255,64,64)
+    g.arc( cx , cy-7 ,3 ,0 , 2*Math.PI )
+    g.fill()
+
+    g.beginPath()
+    g.moveTo cx,cy
+    g.lineTo(xx,yy) for [xx,yy] in [
+      [cx-4  , cy-3 ]
+      [cx+4  , cy-3 ]
+    ]
+    g.lineTo cx,cy
+    g.fill()
+
+class TileSprite extends CanvasSprite
+  shape: (g)->
+    g.init(Color.Black)
+    g.moveTo 0,16
+    g.lineTo(x,y) for [x,y] in [
+      [16,24] , [32,16], [16,8]
+    ]
+    g.lineTo 0,16
+    g.fill()
+
+  draw:(g,x,y)->
+    g.drawImage(@img, x,y)
+
+class GroundSprite extends CanvasSprite
+  constructor:(@map , @size=32)->
+    @ip = [800,1600]
+    [mx,my] = [@map.length*@size , @map[0].length*@size]
+    gr = document.createElement('canvas')
+    gr.width = 32*100
+    gr.height = 32*100
+
+    up = document.createElement('canvas')
+    up.width = 32*100
+    up.height = 32*100
+
+    @shape gr.getContext('2d'),up.getContext('2d')
+    @ground = new Image
+    @ground.src = gr.toDataURL()
+
+    @upper = new Image
+    @upper.src = up.toDataURL()
+
+  p2ism : (x,y)->
+    [ix,iy]= @ip
+    [(x+y)/2+ix
+     (x-y)/4+iy
+    ]
+
+  shape: (g,u)->
+    h = 32
+    for i in [0 ... @map.length]
+      for _j in [0 ... @map[i].length]
+        j = @map[i].length - _j - 1 
+        [vx,vy] = @p2ism i*@size ,j*@size
+        unless @map[i][j] # 通路
+          g.init Color.i(192,192,192)
+          g.moveTo vx,vy
+          g.lineTo(x,y) for [x,y] in [
+            [vx+16,vy+8],[vx+32,vy],[vx+16,vy-8]
+          ]
+          g.lineTo vx,vy
+          g.fill()
+        else # 壁
+          u.init Color.i(64,64,64)
+          # 動的なハイト生成パターン
+          # h = ~~(3+Math.random()*2)*16
+          # h = ~~(j/@map[0].length*20)*8
+          # 上の壁
+          uy = vy - h
+          u.moveTo vx,uy
+          u.lineTo(x,y) for [x,y] in [
+            [vx+16,uy+8],[vx+32,uy],[vx+16,uy-8]
+          ]
+          u.lineTo vx,uy
+          u.fill()
+
+          # 左柱
+          g.init Color.i(62,62,62)
+          g.moveTo vx,vy
+          g.lineTo(x,y) for [x,y] in [
+            [vx,vy-h],[vx+16,vy-h],[vx+16,vy+8]
+          ]
+          g.lineTo vx,vy
+          g.fill()
+
+          # 右柱
+          g.init Color.i(48,48,48)
+          g.moveTo vx+16,vy+8
+          g.lineTo(x,y) for [x,y] in [
+            [vx+16,vy+8-h],[vx+32,vy-h],[vx+32,vy]
+          ]
+          g.lineTo vx+16,vy+8
+          g.fill()
+
+
+  draw:(g,cx,cy)->
+    [ix,iy]= @ip
+    g.drawImage(@ground, cx-320+ix, cy+iy-240, 640, 480, 0 , 0 , 640, 480)
+  draw_upper:(g,cx,cy)->
+    [ix,iy]= @ip
+    g.drawImage(@upper, cx-320+ix, cy+iy-240, 640, 480, 0 , 0 , 640, 480)
+
+
+class GameRenderer
+  constructor : (id,width,height)->
+    # @map = null
+    @uid = null
+    @cam = [0,0]
+    @_camn = [0,0]
+    @mouse = [0,0]
+    @scale = 32
+    @canvas =  document.getElementById "game"
+    @g = @canvas.getContext '2d'
+
+    @canvas.width = 640
+    @canvas.height = 480
+
+    @player_sp = new PlayerSprite 32
+    @char_sp = new CharSprite 32
+    @monster_sp = new MonsterSprite 32
+    @tile_sp = new TileSprite 32
+
+    window.onkeydown = (e)->
+      e.preventDefault()
+      key = getkey(e.keyCode)
+      soc.emit "keydown",code:key
+
+    window.onkeyup = (e)->
+      e.preventDefault()
+      key = getkey(e.keyCode)
+      soc.emit "keyup", code:key
+
+    @canvas.onmousedown = (e)=>
+      console.log [x,y] = @ism2pos e.offsetX,e.offsetY
+      soc.emit "click_map", x:x,y:y
+
+  create_map:(map)->
+    @gr_sp = new GroundSprite map ,32
+
+  to_ism_native : (x,y)->
+    [(x+y)/2
+     (x-y)/4
+    ]
+
+  to_ism : (x,y)->
+    [cx,cy] = @cam
+    [
+     320-cx+(x+y)/2
+     240-cy+(x-y)/4
+    ]
+
+  ism2pos : (x,y)->
+    [dx,dy] = [x-320,y-240]
+    [cx,cy] = @_camn
+    [
+      cx+dx+2*dy
+      cy+dx-2*dy
+    ]
+
+  render : (data={})->
+    # 扱うオブジェクト
+    objs = data.objs
+    for i in objs
+      [x,y,id,oid] = i.o
+      if id is @uid
+        @_camn = [x,y]
+        @cam = @to_ism_native(x,y)
+        break
+
+    # 初期化
+    @g.clearRect(0,0,640,480)
+
+    [cx,cy] = @cam
+
+    @gr_sp?.draw(@g,cx,cy)
+    for i in objs
+      [x,y,id, oid] = i.o
+      {n,hp,lv} = i.s
+      [vx,vy] = @to_ism( x/32*@scale,y/32*@scale)
+      if -64 < vx < 706 and -48 < vy < 528
+        if id is @uid 
+          @player_sp.draw(@g,vx,vy)
+        if id > 1000
+          @char_sp.draw(@g,vx,vy)
+        else 
+          @monster_sp.draw(@g,vx,vy)
+        @g.init Color.Black
+        @g.fillText ''+~~(hp) , vx-6,vy-12
+        @g.fillText n , vx+5,vy
+    @gr_sp?.draw_upper(@g,cx,cy)
+
---
+++ public/game.js
@@ -0,0 +1,490 @@
+var Canvas, CanvasSprite, CharSprite, Color, GameRenderer, GroundSprite, ImageSprite, MonsterSprite, PlayerSprite, Sprite, TileSprite, Util, abs, cos, include, sin, sqrt;
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+  function ctor() { this.constructor = child; }
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  child.__super__ = parent.prototype;
+  return child;
+};
+Array.prototype.remove = function(obj) {
+  return this.splice(this.indexOf(obj), 1);
+};
+Array.prototype.size = function() {
+  return this.length;
+};
+Array.prototype.first = function() {
+  return this[0];
+};
+Array.prototype.last = function() {
+  return this[this.length - 1];
+};
+Array.prototype.each = Array.prototype.forEach;
+cos = Math.cos, sin = Math.sin, sqrt = Math.sqrt, abs = Math.abs;
+Color = {
+  Rンed: "rgb(255,0,0)",
+  Blue: "rgb(0,0,255)",
+  Green: "rgb(0,255,0)",
+  White: "rgb(255,255,255)",
+  Black: "rgb(0,0,0)",
+  i: function(r, g, b) {
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+};
+Canvas = CanvasRenderingContext2D;
+Canvas.prototype.init = function(color, alpha) {
+  if (color == null) {
+    color = Color.i(255, 255, 255);
+  }
+  if (alpha == null) {
+    alpha = 1;
+  }
+  this.beginPath();
+  this.strokeStyle = color;
+  this.fillStyle = color;
+  return this.globalAlpha = alpha;
+};
+Canvas.prototype.initText = function(size, font) {
+  if (size == null) {
+    size = 10;
+  }
+  if (font == null) {
+    font = 'Arial';
+  }
+  return this.font = "" + size + "pt " + font;
+};
+Canvas.prototype.drawLine = function(x, y, dx, dy) {
+  this.moveTo(x, y);
+  this.lineTo(x + dx, y + dy);
+  return this.stroke();
+};
+Canvas.prototype.drawPath = function(fill, path) {
+  var px, py, sx, sy, _ref, _ref2;
+  _ref = path.shift(), sx = _ref[0], sy = _ref[1];
+  this.moveTo(sx, sy);
+  while (path.size() > 0) {
+    _ref2 = path.shift(), px = _ref2[0], py = _ref2[1];
+    this.lineTo(px, py);
+  }
+  this.lineTo(sx, sy);
+  if (fill) {
+    return this.fill();
+  } else {
+    return this.stroke();
+  }
+};
+Canvas.prototype.drawDiffPath = function(fill, path) {
+  var dx, dy, px, py, sx, sy, _ref, _ref2, _ref3, _ref4;
+  _ref = path.shift(), sx = _ref[0], sy = _ref[1];
+  this.moveTo(sx, sy);
+  _ref2 = [sx, sy], px = _ref2[0], py = _ref2[1];
+  while (path.size() > 0) {
+    _ref3 = path.shift(), dx = _ref3[0], dy = _ref3[1];
+    _ref4 = [px + dx, py + dy], px = _ref4[0], py = _ref4[1];
+    this.lineTo(px, py);
+  }
+  this.lineTo(sx, sy);
+  if (fill) {
+    return this.fill();
+  } else {
+    return this.stroke();
+  }
+};
+Canvas.prototype.drawLine = function(x, y, dx, dy) {
+  this.moveTo(x, y);
+  this.lineTo(x + dx, y + dy);
+  return this.stroke();
+};
+Canvas.prototype.drawDLine = function(x1, y1, x2, y2) {
+  this.moveTo(x1, y1);
+  this.lineTo(x2, y2);
+  return this.stroke();
+};
+Canvas.prototype.drawArc = function(fill, x, y, size, from, to, reverse) {
+  if (from == null) {
+    from = 0;
+  }
+  if (to == null) {
+    to = Math.PI * 2;
+  }
+  if (reverse == null) {
+    reverse = false;
+  }
+  this.arc(x, y, size, from, to, reverse);
+  if (fill) {
+    return this.fill();
+  } else {
+    return this.stroke();
+  }
+};
+Util = {};
+Util.prototype = {
+  extend: function(obj, mixin) {
+    var method, name;
+    for (name in mixin) {
+      method = mixin[name];
+      obj[name] = method;
+    }
+    return obj;
+  },
+  include: function(klass, mixin) {
+    return Util.prototype.extend(klass.prototype, mixin);
+  },
+  dup: function(obj) {
+    var f;
+    f = function() {};
+    f.prototype = obj;
+    return new f;
+  }
+};
+include = Util.prototype.include;
+Sprite = (function() {
+  function Sprite(x, y, scale) {
+    this.x = x != null ? x : 0;
+    this.y = y != null ? y : 0;
+    this.scale = scale != null ? scale : 10;
+  }
+  Sprite.prototype.render = function(g) {
+    g.beginPath();
+    g.arc(this.x, this.y, 15, 0, Math.PI * 2, true);
+    return g.stroke();
+  };
+  Sprite.prototype.get_distance = function(target) {
+    var xd, yd;
+    xd = Math.pow(this.x - target.x, 2);
+    yd = Math.pow(this.y - target.y, 2);
+    return Math.sqrt(xd + yd);
+  };
+  Sprite.prototype.get_relative = function(cam) {
+    var pos;
+    return pos = {
+      vx: 320 + this.x - cam.x,
+      vy: 240 + this.y - cam.y
+    };
+  };
+  Sprite.prototype.find_obj = function(group_id, targets, range) {
+    return targets.filter(__bind(function(t) {
+      return t.group === group_id && this.get_distance(t) < range;
+    }, this));
+  };
+  return Sprite;
+})();
+ImageSprite = (function() {
+  function ImageSprite(src, size) {
+    this.size = size;
+    this.img = new Image;
+    this.img.src = src;
+  }
+  ImageSprite.prototype.draw = function(g) {
+    return g.drawImage(this.img, x, y);
+  };
+  return ImageSprite;
+})();
+CanvasSprite = (function() {
+  function CanvasSprite(size) {
+    var buffer;
+    this.size = size != null ? size : 32;
+    buffer = document.createElement('canvas');
+    buffer.width = buffer.height = this.size;
+    this.shape(buffer.getContext('2d'));
+    this.img = new Image;
+    this.img.src = buffer.toDataURL();
+  }
+  CanvasSprite.prototype.p2ism = function(x, y) {
+    return [(x + y) / 2, (x - y) / 4];
+  };
+  CanvasSprite.prototype.draw = function(g, x, y) {
+    var dx, dy;
+    dx = dy = ~~(this.size / 2);
+    return g.drawImage(this.img, x - dx, y - dy);
+  };
+  return CanvasSprite;
+})();
+CharSprite = (function() {
+  __extends(CharSprite, CanvasSprite);
+  function CharSprite() {
+    CharSprite.__super__.constructor.apply(this, arguments);
+  }
+  CharSprite.prototype.shape = function(g) {
+    var cx, cy, xx, yy, _i, _len, _ref, _ref2;
+    cx = cy = 16;
+    g.init(Color.i(64, 255, 64));
+    g.arc(cx, cy - 7, 3, 0, 2 * Math.PI);
+    g.fill();
+    g.beginPath();
+    g.moveTo(cx, cy);
+    _ref = [[cx - 4, cy - 3], [cx + 4, cy - 3]];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      _ref2 = _ref[_i], xx = _ref2[0], yy = _ref2[1];
+      g.lineTo(xx, yy);
+    }
+    g.lineTo(cx, cy);
+    return g.fill();
+  };
+  return CharSprite;
+})();
+PlayerSprite = (function() {
+  __extends(PlayerSprite, CanvasSprite);
+  function PlayerSprite() {
+    PlayerSprite.__super__.constructor.apply(this, arguments);
+  }
+  PlayerSprite.prototype.shape = function(g) {
+    var cx, cy, xx, yy, _i, _len, _ref, _ref2;
+    cx = cy = 16;
+    g.init(Color.i(64, 64, 255));
+    g.arc(cx, cy - 7, 3, 0, 2 * Math.PI);
+    g.fill();
+    g.beginPath();
+    g.moveTo(cx, cy);
+    _ref = [[cx - 4, cy - 3], [cx + 4, cy - 3]];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      _ref2 = _ref[_i], xx = _ref2[0], yy = _ref2[1];
+      g.lineTo(xx, yy);
+    }
+    g.lineTo(cx, cy);
+    return g.fill();
+  };
+  return PlayerSprite;
+})();
+MonsterSprite = (function() {
+  __extends(MonsterSprite, CanvasSprite);
+  function MonsterSprite() {
+    MonsterSprite.__super__.constructor.apply(this, arguments);
+  }
+  MonsterSprite.prototype.shape = function(g, color) {
+    var cx, cy, xx, yy, _i, _len, _ref, _ref2;
+    cx = cy = 16;
+    g.init(color || Color.i(255, 64, 64));
+    g.arc(cx, cy - 7, 3, 0, 2 * Math.PI);
+    g.fill();
+    g.beginPath();
+    g.moveTo(cx, cy);
+    _ref = [[cx - 4, cy - 3], [cx + 4, cy - 3]];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      _ref2 = _ref[_i], xx = _ref2[0], yy = _ref2[1];
+      g.lineTo(xx, yy);
+    }
+    g.lineTo(cx, cy);
+    return g.fill();
+  };
+  return MonsterSprite;
+})();
+TileSprite = (function() {
+  __extends(TileSprite, CanvasSprite);
+  function TileSprite() {
+    TileSprite.__super__.constructor.apply(this, arguments);
+  }
+  TileSprite.prototype.shape = function(g) {
+    var x, y, _i, _len, _ref, _ref2;
+    g.init(Color.Black);
+    g.moveTo(0, 16);
+    _ref = [[16, 24], [32, 16], [16, 8]];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      _ref2 = _ref[_i], x = _ref2[0], y = _ref2[1];
+      g.lineTo(x, y);
+    }
+    g.lineTo(0, 16);
+    return g.fill();
+  };
+  TileSprite.prototype.draw = function(g, x, y) {
+    return g.drawImage(this.img, x, y);
+  };
+  return TileSprite;
+})();
+GroundSprite = (function() {
+  __extends(GroundSprite, CanvasSprite);
+  function GroundSprite(map, size) {
+    var gr, mx, my, up, _ref;
+    this.map = map;
+    this.size = size != null ? size : 32;
+    this.ip = [800, 1600];
+    _ref = [this.map.length * this.size, this.map[0].length * this.size], mx = _ref[0], my = _ref[1];
+    gr = document.createElement('canvas');
+    gr.width = 32 * 100;
+    gr.height = 32 * 100;
+    up = document.createElement('canvas');
+    up.width = 32 * 100;
+    up.height = 32 * 100;
+    this.shape(gr.getContext('2d'), up.getContext('2d'));
+    this.ground = new Image;
+    this.ground.src = gr.toDataURL();
+    this.upper = new Image;
+    this.upper.src = up.toDataURL();
+  }
+  GroundSprite.prototype.p2ism = function(x, y) {
+    var ix, iy, _ref;
+    _ref = this.ip, ix = _ref[0], iy = _ref[1];
+    return [(x + y) / 2 + ix, (x - y) / 4 + iy];
+  };
+  GroundSprite.prototype.shape = function(g, u) {
+    var h, i, j, uy, vx, vy, x, y, _j, _ref, _results;
+    h = 32;
+    _results = [];
+    for (i = 0, _ref = this.map.length; 0 <= _ref ? i < _ref : i > _ref; 0 <= _ref ? i++ : i--) {
+      _results.push((function() {
+        var _ref2, _ref3, _results2;
+        _results2 = [];
+        for (_j = 0, _ref2 = this.map[i].length; 0 <= _ref2 ? _j < _ref2 : _j > _ref2; 0 <= _ref2 ? _j++ : _j--) {
+          j = this.map[i].length - _j - 1;
+          _ref3 = this.p2ism(i * this.size, j * this.size), vx = _ref3[0], vy = _ref3[1];
+          _results2.push((function() {
+            var _i, _k, _l, _len, _len2, _len3, _len4, _m, _ref10, _ref11, _ref4, _ref5, _ref6, _ref7, _ref8, _ref9;
+            if (!this.map[i][j]) {
+              g.init(Color.i(192, 192, 192));
+              g.moveTo(vx, vy);
+              _ref4 = [[vx + 16, vy + 8], [vx + 32, vy], [vx + 16, vy - 8]];
+              for (_i = 0, _len = _ref4.length; _i < _len; _i++) {
+                _ref5 = _ref4[_i], x = _ref5[0], y = _ref5[1];
+                g.lineTo(x, y);
+              }
+              g.lineTo(vx, vy);
+              return g.fill();
+            } else {
+              u.init(Color.i(64, 64, 64));
+              uy = vy - h;
+              u.moveTo(vx, uy);
+              _ref6 = [[vx + 16, uy + 8], [vx + 32, uy], [vx + 16, uy - 8]];
+              for (_k = 0, _len2 = _ref6.length; _k < _len2; _k++) {
+                _ref7 = _ref6[_k], x = _ref7[0], y = _ref7[1];
+                u.lineTo(x, y);
+              }
+              u.lineTo(vx, uy);
+              u.fill();
+              g.init(Color.i(62, 62, 62));
+              g.moveTo(vx, vy);
+              _ref8 = [[vx, vy - h], [vx + 16, vy - h], [vx + 16, vy + 8]];
+              for (_l = 0, _len3 = _ref8.length; _l < _len3; _l++) {
+                _ref9 = _ref8[_l], x = _ref9[0], y = _ref9[1];
+                g.lineTo(x, y);
+              }
+              g.lineTo(vx, vy);
+              g.fill();
+              g.init(Color.i(48, 48, 48));
+              g.moveTo(vx + 16, vy + 8);
+              _ref10 = [[vx + 16, vy + 8 - h], [vx + 32, vy - h], [vx + 32, vy]];
+              for (_m = 0, _len4 = _ref10.length; _m < _len4; _m++) {
+                _ref11 = _ref10[_m], x = _ref11[0], y = _ref11[1];
+                g.lineTo(x, y);
+              }
+              g.lineTo(vx + 16, vy + 8);
+              return g.fill();
+            }
+          }).call(this));
+        }
+        return _results2;
+      }).call(this));
+    }
+    return _results;
+  };
+  GroundSprite.prototype.draw = function(g, cx, cy) {
+    var ix, iy, _ref;
+    _ref = this.ip, ix = _ref[0], iy = _ref[1];
+    return g.drawImage(this.ground, cx - 320 + ix, cy + iy - 240, 640, 480, 0, 0, 640, 480);
+  };
+  GroundSprite.prototype.draw_upper = function(g, cx, cy) {
+    var ix, iy, _ref;
+    _ref = this.ip, ix = _ref[0], iy = _ref[1];
+    return g.drawImage(this.upper, cx - 320 + ix, cy + iy - 240, 640, 480, 0, 0, 640, 480);
+  };
+  return GroundSprite;
+})();
+GameRenderer = (function() {
+  function GameRenderer(id, width, height) {
+    this.uid = null;
+    this.cam = [0, 0];
+    this._camn = [0, 0];
+    this.mouse = [0, 0];
+    this.scale = 32;
+    this.canvas = document.getElementById("game");
+    this.g = this.canvas.getContext('2d');
+    this.canvas.width = 640;
+    this.canvas.height = 480;
+    this.player_sp = new PlayerSprite(32);
+    this.char_sp = new CharSprite(32);
+    this.monster_sp = new MonsterSprite(32);
+    this.tile_sp = new TileSprite(32);
+    window.onkeydown = function(e) {
+      var key;
+      e.preventDefault();
+      key = getkey(e.keyCode);
+      return soc.emit("keydown", {
+        code: key
+      });
+    };
+    window.onkeyup = function(e) {
+      var key;
+      e.preventDefault();
+      key = getkey(e.keyCode);
+      return soc.emit("keyup", {
+        code: key
+      });
+    };
+    this.canvas.onmousedown = __bind(function(e) {
+      var x, y, _ref;
+      console.log((_ref = this.ism2pos(e.offsetX, e.offsetY), x = _ref[0], y = _ref[1], _ref));
+      return soc.emit("click_map", {
+        x: x,
+        y: y
+      });
+    }, this);
+  }
+  GameRenderer.prototype.create_map = function(map) {
+    return this.gr_sp = new GroundSprite(map, 32);
+  };
+  GameRenderer.prototype.to_ism_native = function(x, y) {
+    return [(x + y) / 2, (x - y) / 4];
+  };
+  GameRenderer.prototype.to_ism = function(x, y) {
+    var cx, cy, _ref;
+    _ref = this.cam, cx = _ref[0], cy = _ref[1];
+    return [320 - cx + (x + y) / 2, 240 - cy + (x - y) / 4];
+  };
+  GameRenderer.prototype.ism2pos = function(x, y) {
+    var cx, cy, dx, dy, _ref, _ref2;
+    _ref = [x - 320, y - 240], dx = _ref[0], dy = _ref[1];
+    _ref2 = this._camn, cx = _ref2[0], cy = _ref2[1];
+    return [cx + dx + 2 * dy, cy + dx - 2 * dy];
+  };
+  GameRenderer.prototype.render = function(data) {
+    var cx, cy, hp, i, id, lv, n, objs, oid, vx, vy, x, y, _i, _j, _len, _len2, _ref, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7;
+    if (data == null) {
+      data = {};
+    }
+    objs = data.objs;
+    for (_i = 0, _len = objs.length; _i < _len; _i++) {
+      i = objs[_i];
+      _ref = i.o, x = _ref[0], y = _ref[1], id = _ref[2], oid = _ref[3];
+      if (id === this.uid) {
+        this._camn = [x, y];
+        this.cam = this.to_ism_native(x, y);
+        break;
+      }
+    }
+    this.g.clearRect(0, 0, 640, 480);
+    _ref2 = this.cam, cx = _ref2[0], cy = _ref2[1];
+    if ((_ref3 = this.gr_sp) != null) {
+      _ref3.draw(this.g, cx, cy);
+    }
+    for (_j = 0, _len2 = objs.length; _j < _len2; _j++) {
+      i = objs[_j];
+      _ref4 = i.o, x = _ref4[0], y = _ref4[1], id = _ref4[2], oid = _ref4[3];
+      _ref5 = i.s, n = _ref5.n, hp = _ref5.hp, lv = _ref5.lv;
+      _ref6 = this.to_ism(x / 32 * this.scale, y / 32 * this.scale), vx = _ref6[0], vy = _ref6[1];
+      if ((-64 < vx && vx < 706) && (-48 < vy && vy < 528)) {
+        if (id === this.uid) {
+          this.player_sp.draw(this.g, vx, vy);
+        }
+        if (id > 1000) {
+          this.char_sp.draw(this.g, vx, vy);
+        } else {
+          this.monster_sp.draw(this.g, vx, vy);
+        }
+        this.g.init(Color.Black);
+        this.g.fillText('' + ~~hp, vx - 6, vy - 12);
+        this.g.fillText(n, vx + 5, vy);
+      }
+    }
+    return (_ref7 = this.gr_sp) != null ? _ref7.draw_upper(this.g, cx, cy) : void 0;
+  };
+  return GameRenderer;
+})();
